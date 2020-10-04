cb46f11a9d5b5dfbf1be6f1db0074f9a9857fe30
Cheshta Sagar <cheshtas22@gmail.com>
2020-10-04 18:00:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//entry point of the project
+var express=require('express');
+
+var app=express();
+//setting up template engine
+app.set('view engine','ejs');
+
+//for using static files
+app.use(express.static('/public'));
+
+
+app.get('/',function(req,res)
+{
+res.send('home page');
+});
+app.listen(3000);
+console.log('server is running at port 3000');
