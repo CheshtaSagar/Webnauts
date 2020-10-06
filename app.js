84e9b951bbb87ef946492a225bc1943283af52bb
Akanksha Verma <akanksha3454verma@gmail.com>
2020-10-06 13:48:41 +0530

    added footer and connected to db

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 //entry point of the project
-var express=require('express');
+var express= require('express');
+var path= require('path');
+var mongoose= require('mongoose');
+var config= require('./config/database');
+var bodyParser= require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var expressMessages = require('express-messages');
+
+//connect to dbs
+mongoose.connect(config.database);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongoDb..');
+});
 
 var app=express();
 
@@ -9,10 +24,8 @@ app.set('view engine','ejs');
 //for using static files
 app.use(express.static('/public'));
 
-app.get('/',function(req,res)
-{
-res.render('index');
-});
+var pages = require('./routes/pages.js');//taking to pages.js in routes
+app.use('/', pages);
 
 app.listen(3000);
 console.log('server is running at port 3000');
